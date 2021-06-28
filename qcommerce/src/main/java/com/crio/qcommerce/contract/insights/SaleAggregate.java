6b0e2dd25f1fc5c177f044d9176f99482f7d4c50
Roby Jacob <robyjacob@Robys-Mac-mini.local>
2021-06-28 16:06:21 +0530

    ME_BUILDOUT_QCOMMERCE_ANALYTICS_MODULE_SALES

--- qcommerce/src/main/java/com/crio/qcommerce/contract/insights/SaleAggregate.java
+++ qcommerce/src/main/java/com/crio/qcommerce/contract/insights/SaleAggregate.java
@@ -13,4 +13,3 @@ public class SaleAggregate {
   private List<SaleAggregateByMonth> aggregateByMonths;
 
 }
-
--- qcommerce/src/main/java/com/crio/qcommerce/contract/insights/SaleAggregateByMonth.java
+++ qcommerce/src/main/java/com/crio/qcommerce/contract/insights/SaleAggregateByMonth.java
@@ -13,4 +13,3 @@ public class SaleAggregateByMonth {
   private Integer month;
   private Double sales;
 }
-
